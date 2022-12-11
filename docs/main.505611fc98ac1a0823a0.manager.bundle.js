70983629c6819ce660fd5cb568107064d8fae012
Iulian Radu <iulian.radu@gmx.at>
2022-12-11 15:18:01 +0100

    Updated the storybook

--- docs/main.505611fc98ac1a0823a0.manager.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{502:function(module,exports,__webpack_require__){__webpack_require__(503),module.exports=__webpack_require__(966)},568:function(module,exports){},718:function(module,exports){}},[[502,2,3]]]);
